0878e53dffd2808d3e9729a0e3df5a9e5b49a97c
Dima Kurbatov <dskurbatov13@gmail.com>
2019-06-09 08:06:01 -0700

    add more tests for question generator

--- test/questions.test.js
+++ test/questions.test.js
@@ -1,23 +1,75 @@
-import assert from 'assert'
-import { questions } from '../src/questions.json'
-import { questionGenerator } from '../src/helpers/questions'
+const assert = require('assert')
+const { questions } = require('../src/questions.json')
+
+class QuestionsGen {
+  constructor(questions){
+    this.questions = questions
+    this.id = 1
+  }
+
+  isNext(answer){
+    if(!this.questions[this.id].paths){
+      return false
+    }
+    
+    if(this.questions[this.id].paths !== 'number'){
+      this.id = this.questions[this.id].paths[answer]
+    } else {
+      this.id = this.questions[this.id].paths
+    }
+    
+    if(this.id < 0) {
+      this.id = 0
+    }
+    
+    return true 
+  }
+
+  getNextQuestion(){
+    return this.questions[this.id].question
+  }
+
+  validate(answer){
+    if(typeof this.questions[this.id].validation === 'boolean'){
+      return this.questions[this.id].validation
+    }
+
+    if(Array.isArray(this.questions[this.id].validation)){
+      return this.questions[this.id].validation.includes(answer)
+    }
+
+    if(typeof this.questions[this.id].validation === 'string'){
+      const reg = new RegExp(this.questions[this.id].validation)
+      return reg.test(answer)
+    }
+
+    return false
+  }
+}
 
 describe('questionGenerator', () => {
-  const gen = questionGenerator(questions, 0)
+  const gen = new QuestionsGen(questions)
   it('returns first questions', () => {
-    assert.deepEqual(gen.next().value, questions[0])
+    assert.equal(gen.getNextQuestion(), questions[1].question)
   })
 
-  it('returns next questions based on answer', () => {
-    assert.deepEqual(gen.next('good').value, questions[1])
+  it('should validate the answer for prev question', () => {
+    assert.equal(gen.validate('yes'), true)
+    assert.equal(gen.validate('no'), true)
   })
 
-  it('returns next question based on answer', () => {
-    assert.deepEqual(gen.next('nothing').value, questions[3])
+  it('should validate to false if wrong answer for prev question', () => {
+    assert.equal(gen.validate('not sure'), false)
   })
 
-  it('when there is no more questions set done to true', () => {
-    assert.equal(gen.next().done, true)
+  it('if validation is true check if there is next question', () => {
+    assert.equal(gen.isNext('no'), true)
+    assert.equal(gen.getNextQuestion(), questions[0].question)
+  })
+
+  it('if validation is false return prev question', () => {
+    assert.equal(gen.validate('sorry'), false)
+    assert.equal(gen.getNextQuestion(), questions[0].question)
   })
 
 })
