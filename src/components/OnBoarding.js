05dc55d672b666473047bbfc936e902839c10981
Dima Kurbatov <dskurbatov13@gmail.com>
2019-06-09 08:06:36 -0700

    redesign OnBoarding component

--- src/components/OnBoarding.js
+++ src/components/OnBoarding.js
@@ -1,32 +1,52 @@
 import React from 'react'
 import List from './List'
 import Form from './Form'
-import { questionGenerator } from '../helpers/questions'
+import { QuestionsGen } from '../helpers/questions'
 import { questions } from '../questions.json'
 
-const gen = questionGenerator(questions, 0)
-
-const firstValue = gen.next().value
+const gen = new QuestionsGen(questions)
 
 export default function OnBoarding (props) {
-  const [messages, setMessages] = React.useState([firstValue.question])
+  const [messages, setMessages] = React.useState([gen.getNextQuestion()])
   
   const addMessage = (message) => {
-    const nMessages = messages.map(item => item)
-    nMessages.push(message)
-    let value = gen.next(message).value
-    if(!value){
-      props.setStatus('pationt')
+    if (gen.validate(message) && gen.isNext(message)){
+      setMessages([
+        ...messages,
+        message,
+        gen.getNextQuestion()
+      ])
+      // setTimeout(() => {
+      //   console.log(messages)
+      //   const nMessages = messages.map(item => item)
+      //   nMessages.push(gen.getNextQuestion())
+      //   setMessages(nMessages)
+      // }, 1000)
     } else {
-      nMessages.push(value.question)
+      setMessages([
+        ...messages,
+        message
+      ])
     }
-    setMessages(nMessages)
   }
+
+  const getDOMElement = (el) => el
+  // const addMessage = (message) => {
+  //   const nMessages = messages.map(item => item)
+  //   nMessages.push(message)
+  //   let value = gen.next(message).value
+  //   if(!value){
+  //     props.setStatus('pationt')
+  //   } else {
+  //     nMessages.push(value.question)
+  //   }
+  //   setMessages(nMessages) 
+  // }
   
   return (
     <React.Fragment>
-      <List messages={messages} />
-      <Form onMessage={addMessage} />
+      <List messages={messages} onMessage={addMessage}/>
+      {/* <Form onMessage={addMessage} getDOM={getDOMElement}/> */}
     </React.Fragment>
   )
 }
