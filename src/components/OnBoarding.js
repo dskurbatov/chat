c72577625f7762cc598ad69d6bcf1206e9881b05
Dima Kurbatov <dskurbatov13@gmail.com>
2019-06-09 17:09:04 -0700

    redesign OnBoarding component to use redux

--- src/components/OnBoarding.js
+++ src/components/OnBoarding.js
@@ -1,67 +1,114 @@
 import React from 'react'
-import List from './List'
+import PropTypes from 'prop-types'
 import Form from './Form'
 import { QuestionsGen } from '../helpers/questions'
 import { questions } from '../questions.json'
+import { 
+  connectToSocket,
+  lastQuestion,
+  validAnswer,
+  notValidAnswer,
+  nextQuestion,
+  firstQuestion
+ } from '../redux/actions'
 
-const gen = new QuestionsGen(questions)
+const jsonUrl = 'https://gist.githubusercontent.com/pcperini/97fe41fc42ac1c610548cbfebb0a4b88/raw/cc07f09753ad8fefb308f5adae15bf82c7fffb72/cerebral_challenge.json'
+
+let gen = null
 
 export default class OnBoarding extends React.Component {
-  state = {
-    messages: [gen.getNextQuestion()],
-    isConnected: false,
-    status: 'onboarding'
+
+  addMessage = (message) => {
+    const { store } = this.context
+    
+    if(gen.validate(message) && gen.isNext(message)){
+      const id = gen.getCurrentQuestionId()
+      const question = gen.getNextQuestion()
+      
+      store.dispatch(validAnswer(id, message))
+        .then(() => this.focus())
+      
+      if(gen.isLast()){
+        return store.dispatch(lastQuestion(question))
+          .then(() => this.focus())
+      }
+
+      return store.dispatch(nextQuestion(question))
+        .then(() => this.focus())
+    } else {
+      const { questions } = store.getState()
+      const prevQuestion = questions[questions.length - 1]
+    
+      return store.dispatch(notValidAnswer(message, prevQuestion))
+        .then(() => this.focus())
+    }
   }
   
-  addMessage = (message) => {
-    if(gen.validate(message) && gen.isNext(message)){
-      this.setState((oldState, props) => {
-        console.log(gen.isEnd())
-        return {
-          ...oldState,
-          status: (gen.isEnd() ? 'onboard' : oldState.status),
-          messages: [
-            ...oldState.messages,
-            message,
-            gen.getNextQuestion()
-          ]
-        }
-      }, this.focus)
-    }
+  componentDidMount(){
+    fetch(jsonUrl)
+      .then(res => res.json())
+      .then(questions => gen = new QuestionsGen(questions))
+      .then(() => {
+        const { store } = this.context
+        const question = gen.getNextQuestion()
+        
+        store.dispatch(firstQuestion(question))
+      })
   }
 
   connect = () => {
+    const { store } = this.context
     const socket = new WebSocket('wss://echo.websocket.org')
 
     socket.onopen = () => {
-      this.setState((oldState, props) => {
-        return {
-          ...oldState,
-          isConnected: true,
-          messages: [
-            ...oldState.messages,
-            'Doctor is now connected'
-          ]
-        }
-      }, this.focus)
+      store.dispatch(connectToSocket).then(() => this.focus())
     }
   }
 
   focus = () => {
-    this.refs[this.state.messages.length - 1].scrollIntoView()
+    const len = Object.keys(this.refs).length
+    this.refs[len - 1].scrollIntoView()
+  }
+
+  buildMessages = (questions, answers) => {
+    const messages = []
+    let i = 0, j = 0
+    while(i < questions.length && j < answers.length){
+      messages.push(questions[i], answers[j])
+      i++;
+      j++
+    }
+    while(i < questions.length){
+      messages.push(questions[i])
+      i++;
+    }
+    while(j < answers.length){
+      messages.push(answers[j])
+      j++
+    }
+    return messages
   }
   
   render(){
+    const { store } = this.context
+    const { questions, answers, isTyping, isPatient, isConnected } = store.getState()
+    const messages = this.buildMessages(questions, answers)
+    
     return (
       <React.Fragment>
         <ul>
-          {this.state.messages.map((message, index) => {
+          {messages.map((message, index) => {
             return <li key={index} ref={index}>{message}</li>
           })}
         </ul>
-        {this.state.status === 'onboard' && !this.state.isConnected && <button onClick={this.connect}>Connect To Doctor</button>}
-        <Form onMessage={this.addMessage} />
+        <p>{isTyping ? 'typing...' : ''}</p>
+        {isPatient && !isConnected && <button onClick={this.connect}>Connect To Doctor</button>}
+       <Form onMessage={this.addMessage}/> 
       </React.Fragment>
     )
   }
+}
+
+OnBoarding.contextTypes = {
+  store: PropTypes.object
 }
